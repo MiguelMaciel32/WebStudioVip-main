c15c0e3554ee097933f850041a9154ffc1093317
Luís Miguel <157251097+MiguelMaciel32@users.noreply.github.com>
2024-11-19 20:31:56 -0300

    att

--- app/api/create-preference/route.ts
+++ app/api/create-preference/route.ts
@@ -1,7 +1,6 @@
 import Stripe from "stripe";
 import { NextRequest, NextResponse } from "next/server";
 
-// Inicializa o Stripe com a chave secreta
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string, {
   apiVersion: "2024-09-30.acacia",
 });
